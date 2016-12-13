eb546271679fe533a850609f7a771acc9996613f
idemitsu <ma15012@shibaura-it.ac.jp>
2016-12-14 03:04:14 +0900

    change file

--- .ipynb_checkpoints/Tmain.py
+++ .ipynb_checkpoints/Tmain.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 
 from Cases_extract import Cases_extract
 from pandas import DataFrame
@@ -6,7 +7,8 @@ import pandas as pd
 import itertools
 import pickle
 import math
-
+from Deepcase import Deepcase
+import re
 
 def tf(terms, document):
     # TF値の計算。単語リストと文章を渡す
@@ -26,8 +28,15 @@ def tf_idf(terms, documents):
 
 if __name__ == "__main__":
     # 全トリプルの抽出
-    Ce= Cases_extract()
-    # '''
+
+    netpath = "D:/tmp/Evaluation/neural_network/neuron7/Trained.Network"
+    dummylistpath = "D:/tmp/Evaluation/dummylist.Word"
+    NV_classpath = "D:/tmp/Evaluation/NV_class.Word"
+    Dc = Deepcase(netpath, dummylistpath, NV_classpath)
+
+    Ce= Cases_extract(Dc)
+
+    #'''
     path = u'D:/Users/ide/Desktop/データ関連/診断考察/report_data_ver4_2.xlsx'
     path = u'D:/研究/データ/report_data_ver4_2.xlsx'
     triplelist = Ce.Triple_extract(path)
@@ -52,12 +61,6 @@ if __name__ == "__main__":
     #'''
     # トリプルから事象の抽出
 
-    from Deepcase import Deepcase
-
-    netpath = "D:/tmp/Evaluation/neural_network/neuron7/Trained.Network"
-    dummylistpath = "D:/tmp/Evaluation/dummylist.Word"
-    NV_classpath = "D:/tmp/Evaluation/NV_class.Word"
-    Dc = Deepcase(netpath, dummylistpath, NV_classpath)
     #'''
     tripleFrame = pd.read_csv("D:/tmp/Treport/Triple.csv", encoding='shift-jis')
     tripleFrame_Treport = Ce.TNoun_extract(tripleFrame, Dc.NV_class)
@@ -78,66 +81,7 @@ if __name__ == "__main__":
     #'''
     #格フレームの構築
     tripleFrame_Treport = pd.read_csv("D:/tmp/Treport/Triple_Treport.csv", encoding='shift-jis')
-    Result_input = []
-    Result_output = []
-    DeepCaseList = [u"主体", u"起点", u"対象", u"状況", u"着点", u"手段", u"関係"]
-
-    DeepCase_Noun_perV = [[] for i in range(len(DeepCaseList)+1)]
-    #SurfaceCase_Noun_perV = [[] for i in range(len(Dc.dummylist[2].columns)+1)]
-
-    DeepCase_Noun = [[] for i in range(len(DeepCaseList)+1)]
-    #SurfaceCase_Noun = [[] for i in range(len(Dc.dummylist[2].columns)+1)]
-    Verb_target = []
-    Verb_target_id = []
-    for Report_id in tripleFrame_Treport[u"報告書_id"].drop_duplicates():
-        #if list(tripleFrame_Treport.index).count(triple_index)<=1:
-        #    continue
-        tripleFrame_Treport_sort = tripleFrame_Treport.ix[tripleFrame_Treport[u"報告書_id"]==Report_id,:].sort_index(by=[u"文_id",u"動詞_id"])
-        for SV_id in Series(zip(tripleFrame_Treport_sort[u"文_id"], tripleFrame_Treport_sort[u"動詞_id"])).drop_duplicates():
-            for index_perF, triple_perF in enumerate(tripleFrame_Treport_sort[(tripleFrame_Treport_sort[u"文_id"]==SV_id[0]) & (tripleFrame_Treport_sort[u"動詞_id"]==SV_id[1])].loc[:,[u"名詞",u"助詞",u"動詞"]].values):
-                Noun = triple_perF[0]
-                Particle = triple_perF[1]
-                Verb = triple_perF[2]
-                if Noun!=Noun:
-                    continue
-                print Report_id, SV_id[0], SV_id[1]
-                Result = Dc.predict(Noun, Particle, Verb)
-                DeepCase_unique = Dc.identify(Result)
-                #print Noun, Particle, Verb, DeepCase_unique
-
-                DeepCase_Noun_perV[DeepCaseList.index(DeepCase_unique)].append(Noun)
-
-                if index_perF == len(tripleFrame_Treport_sort[(tripleFrame_Treport_sort[u"文_id"]==SV_id[0]) & (tripleFrame_Treport_sort[u"動詞_id"]==SV_id[1])])-1:
-                    while [] in DeepCase_Noun_perV:
-                        DeepCase_Noun_perV[DeepCase_Noun_perV.index([])] = [u" "]
-
-                    for DeepCase_Noun_tmp in list(itertools.product(DeepCase_Noun_perV[0], DeepCase_Noun_perV[1], DeepCase_Noun_perV[2],DeepCase_Noun_perV[3], DeepCase_Noun_perV[4], DeepCase_Noun_perV[5],DeepCase_Noun_perV[6])):
-                        Verb_target.append(Verb)
-                        Verb_target_id.append((Report_id, SV_id[0], SV_id[1]))
-                        for Di in range(len(DeepCaseList)):
-                            DeepCase_Noun[Di].append(DeepCase_Noun_tmp[Di])
-
-                        DeepCase_Noun_perV = [[] for i in range(len(DeepCaseList))]
-
-    cf_list =[
-    (u"報告書_id", [i[0] for i in Verb_target_id]), (u"文_id", [i[1] for i in Verb_target_id]), (u"動詞_id", [i[2] for i in Verb_target_id]), (u"動詞", Verb_target)
-    ]
-    cf_list.extend([(DeepCaseList[i], DeepCase_Noun[i]) for i in range(len(DeepCaseList))])
-    #cf_list.extend([(Dc.dummylist[2].columns[i], SurfaceCase_Noun[i]) for i in range(len(Dc.dummylist[2].columns))])
-
-    case_frame=dict(cf_list)
-
-    cd_columns = [u"報告書_id", u"文_id", u"動詞_id", u"動詞"]
-    cd_columns.extend([DeepCaseList[i] for i in range(len(DeepCaseList))])
-    #cd_columns.extend([Dc.dummylist[2].columns[i] for i in range(len(Dc.dummylist[2].columns))])
-
-    case_df = DataFrame(case_frame, columns=cd_columns)
-    case_df[u"事象"] = case_df[u"主体"] +" "+ case_df[u"起点"] +" "+ case_df[u"対象"] +" "+ case_df[u"状況"] +" "+ case_df[u"着点"] +" "+ case_df[u"手段"] +" "+ case_df[u"関係"] +" "+ case_df[u"動詞"]
-    for i in case_df.index:
-        case_df.ix[i, u"事象"] = re.sub(r" +", u" ", case_df.ix[i, u"事象"].strip())
-
-    case_df.sort_index(by=[u"報告書_id", u"文_id", u"動詞_id"], inplace=True)
-    #case_df[u"報告書_id"] = [int(i) for i in case_df[u"報告書_id"]]
+    case_df = Ce.create_caseframe(tripleFrame_Treport)
     case_df.to_csv(u"D:/tmp/Treport/caseframe.csv", encoding='shift-jis', index=False)
     #'''
     case_df = pd.read_csv("D:/tmp/Treport/caseframe.csv", encoding='shift-jis')
@@ -177,12 +121,39 @@ if __name__ == "__main__":
 
     case_df_Tcluster = pd.read_csv("D:/tmp/Treport/caseframe_Tcluster.csv", encoding='shift-jis')
 
+    #'''
+    VC_list = case_df_Tcluster[u"動詞"].map(lambda x: Dc.NV_class[1].get(x, ""))
+    Fre_VC_Dc = dict()
+    for i, VC_perV in zip(VC_list.keys(), VC_list.values):
+        Dc_count_tmp = [int(x) for x in case_df_Tcluster.ix[i, Dc.DeepCaseList] != u" "]
+        for VC in VC_perV:
+            if VC in Fre_VC_Dc.keys():
+                Fre_VC_Dc[VC] = [x + y for x, y in zip(Fre_VC_Dc[VC], Dc_count_tmp)]
+            else:
+                Fre_VC_Dc[VC] = Dc_count_tmp
+    times = 1.5
+    VC_Dc = dict()
+    for i in Fre_VC_Dc.keys():
+        TW = sum(Fre_VC_Dc[i]) * (1 / float(len(Fre_VC_Dc[i]))) * times
+        Dc_perVc = [Dc.DeepCaseList[Fre_VC_Dc[i].index(x)] for x in Fre_VC_Dc[i] if x > TW]
+        VC_Dc[i] = Dc_perVc
+    file = open('D:/tmp/Treport/VC_Dc.list', 'w')
+    pickle.dump(VC_Dc, file)
+    file.close()
+    #'''
+
+
+    file = open('D:/tmp/Treport/VC_Dc.list')
+    VC_Dc = pickle.load(file)
+    file.close()
+
+
     dist_method = u"Jaccard"
     # dist_method = u"Simpson"
     threshould_dist = 0.4
 
     # 確認用のためデータ削減
-    N = 100
+    N = 10
     ExNlist = list(case_df_Tcluster[u"報告書_id"].drop_duplicates()[:N])
     case_df_Tcluster = case_df_Tcluster.ix[case_df_Tcluster[u"報告書_id"].map(lambda x: x in ExNlist), :]
 
@@ -206,10 +177,13 @@ if __name__ == "__main__":
 
 
     Wdist = pd.read_csv("D:/tmp/Treport/Wdist.csv", encoding='shift-jis')
-    case_mat = pd.pivot_table(case_df_unified.loc[:, [u"報告書_id", u"事象"]], index=u"報告書_id", columns=u"事象",
+    case_mat = pd.pivot_table(case_df_unified.loc[:, [u"レコード_id", u"事象"]], index=u"レコード_id", columns=u"事象",
                               aggfunc=len).fillna(0)
     case_mat[case_mat > 1] = 1
     case_mat.to_csv("D:/tmp/Treport/case_mat.csv", encoding='shift-jis', index=False)
+    #出現頻度1の事象を抽出
+    Fre1_cases = case_mat.columns[case_mat.sum() == 1]
+    #出現頻度1以上の事象を抽出
     indexer = case_mat.sum() > 1
     case_mat2 = case_mat[indexer.index[indexer]]
     columner = case_mat2.sum(1) > 0
